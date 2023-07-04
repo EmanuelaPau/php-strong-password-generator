6fa8faaf835f8918cdd85822ee49b5a7b692bb05
EmanuelaPau <uapame@gmail.com>
2023-07-04 15:29:44 +0200

    Add bootstrap

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Super strong password generator</title>
+    <!-- CSS -->
+    <link href="./style/style.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <main>
+       <div class="container">
+            <div class="row">
+                <div class="col-12 mt-5">
+                    <h1 class="text-center text-color-grey">
+                        Strong Password Generator
+                    </h1>
+                    <h2 class="text-center text-color-white">
+                        Genera una password sicura
+                    </h2>
+                </div>
+            </div>
+       </div>
+       <div class="container">
+            <form class="row bg-color-grey px-3 py-4 rounded-3 d-flex">
+                <div class="col-6 d-flex align-items-center">
+                    <label for="exampleInputEmail1"><Label>Lunghezza password:</Label>
+                </div>                
+                <div class="col-6">
+                    <input type="email" class="form-control w-50" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
+                </div>
+
+                <div class="col-6 d-flex align-items-center">
+                    <label for="exampleInputEmail1"><Label>Consenti ripetizioni di uno o piu caratteri: </Label>
+                </div>                
+                <div class="col-6">
+                    <input type="email" class="form-control w-50" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
+                </div>
+                    
+                
+            </form>    
+
+       </div>
+    </main>
+</body>
+</html>
